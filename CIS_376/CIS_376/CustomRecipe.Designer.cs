12c9ac70cde8189b4c7db1a30337bbc04f0f3eae
mattj241 <londonm241@gmail.com>
2018-04-15 21:33:23 -0400

    Start to custom recipe
    MAY BE MISSING STUFF

---
+++ CIS_376/CIS_376/CustomRecipe.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CIS_376
+{
+    partial class CustomRecipe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FoodBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AddButton2 = new System.Windows.Forms.Button();
+            this.listyView = new System.Windows.Forms.ListView();
+            this.ReturnButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FoodBox2
+            // 
+            this.FoodBox2.FormattingEnabled = true;
+            this.FoodBox2.Location = new System.Drawing.Point(43, 175);
+            this.FoodBox2.Name = "FoodBox2";
+            this.FoodBox2.Size = new System.Drawing.Size(121, 21);
+            this.FoodBox2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Custom Recipe";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 47);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select the ingredients you want to use in a recipe:\r\n";
+            // 
+            // AddButton2
+            // 
+            this.AddButton2.Location = new System.Drawing.Point(180, 175);
+            this.AddButton2.Name = "AddButton2";
+            this.AddButton2.Size = new System.Drawing.Size(42, 23);
+            this.AddButton2.TabIndex = 3;
+            this.AddButton2.Text = "Add";
+            this.AddButton2.UseVisualStyleBackColor = true;
+            this.AddButton2.Click += new System.EventHandler(this.AddButton2_Click);
+            // 
+            // listyView
+            // 
+            this.listyView.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listyView.Location = new System.Drawing.Point(237, 175);
+            this.listyView.Name = "listyView";
+            this.listyView.Size = new System.Drawing.Size(173, 133);
+            this.listyView.TabIndex = 4;
+            this.listyView.UseCompatibleStateImageBehavior = false;
+            // 
+            // ReturnButton2
+            // 
+            this.ReturnButton2.Location = new System.Drawing.Point(411, 45);
+            this.ReturnButton2.Name = "ReturnButton2";
+            this.ReturnButton2.Size = new System.Drawing.Size(75, 23);
+            this.ReturnButton2.TabIndex = 6;
+            this.ReturnButton2.Text = "Return";
+            this.ReturnButton2.UseVisualStyleBackColor = true;
+            this.ReturnButton2.Click += new System.EventHandler(this.ReturnButton_Click);
+            // 
+            // CustomRecipe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 450);
+            this.Controls.Add(this.ReturnButton2);
+            this.Controls.Add(this.listyView);
+            this.Controls.Add(this.AddButton2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FoodBox2);
+            this.Name = "CustomRecipe";
+            this.Text = "CustomRecipe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox FoodBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button AddButton2;
+        private System.Windows.Forms.ListView listyView;
+        private System.Windows.Forms.Button ReturnButton2;
+    }
+}
---
+++ CIS_376/CIS_376/CustomRecipe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS_376
+{
+    public partial class CustomRecipe : Form
+    {
+        List<String> FoodCollection = new List<string>();
+        public CustomRecipe()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            FillComboBox();
+        }
+
+        private void FillComboBox()
+        {
+            FoodBox2.DataSource = null;
+            FoodBox2.Items.Clear();
+
+            var foods = ManagerHome.mainDatabaseReference.Foods.ToList().Select(p => new { p.Food_ID, p.Food_Name }).ToList();
+            FoodBox2.DataSource = foods.ToList();
+            FoodBox2.ValueMember = "Food_ID";
+            FoodBox2.DisplayMember = "Food_Name";
+        }
+
+        private void ReturnButton_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+        private void AddButton2_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = new ListViewItem(FoodBox2.Text);
+            if (FoodCollection.Contains(item.ToString()))
+            {
+                MessageBox.Show("Already included!");
+            }
+            else
+            {
+                listyView.Items.Add(item);
+                FoodCollection.Add(item.ToString());
+                FoodBox2.Text = "";
+                listyView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+        }
+    }
+}
